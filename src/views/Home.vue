23bdefcc94232d8d71bd9ba2e2d246cf42d5489e
Juliana <julianasouzamelo@live.com>
2021-12-29 10:10:06 -0300

    adicionando home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,25 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <v-app-bar>
+        <v-container>
+            <v-card>
+                <v-card-title>
+                    Teste home page
+                </v-card-title>
+            </v-card>
+        </v-container>
+    </v-app-bar>
 </template>
 
+
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+    data() {
+        
+    },
+    methods: {
+
+    },
+
 }
 </script>
