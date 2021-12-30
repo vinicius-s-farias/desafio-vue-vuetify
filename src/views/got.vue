55df1a76a821a975226e113ccf8c859dc0bb43fe
vinicius-s-farias <vifarias47@gmail.com>
2021-12-30 17:48:08 -0300

    melhorias dos botoes

--- src/views/got.vue
+++ src/views/got.vue
@@ -1,76 +1,68 @@
 <template>
   <div class="Projects">
     <v-app id="inspire">
-      <v-countainer class="my-5">
-        <v-container row class="mb-6">
-            <v-btn
-              small
-              flat
-              color="white"
-              @click="sortBy('title')"
-              slot="activator"
-            >
-              <v-icon left small>mdi-folder</v-icon>
-            </v-btn>
-            <v-btn
-              small
-              flat
-              color="grey"
-              @click="sortBy('person')"
-              slot="activator"
-            >
-              <v-icon left small>mdi-comment-account</v-icon>
-              <span class="caption text-lowercase">By person</span>
-            </v-btn>
-        </v-container>
-
-        <v-card
-          flat
-          class="pa-3"
-          v-for="(casa, i) in casas"
-          :key="i"
-          color="#90A4AE"
-        >
+      <v-container class="#CFD8DC">
         <v-spacer></v-spacer>
-          <v-layout :class="`pa-3 casa ${casa.name}`">
-            <v-flex xs6 md6>
-              <div class="caption black--text">HOUSE</div>
-              <div>{{ casa.name }}</div>
-            </v-flex>
-            <v-flex xs6 sm6 md6>
-              <div class="caption black--text">Region</div>
-              <div>{{ casa.region }}</div>
-            </v-flex>
-          </v-layout>
-          <v-divider></v-divider>
-        </v-card>
-        <v-card
-          flat
-          class="pa-3"
-          v-for="(personagem, i) in personagens"
-          :key="i"
-          color="#B0BEC5"
-        >
-          <v-layout row wrap :class="`pa-3 personagem ${personagem.name}`">
-            <v-flex xs6 sm6 md6>
-              <div dark class="caption black--text">Characters</div>
-              <div>{{ personagem.name }}</div>
-            </v-flex>
-            <v-flex xs6 sm6 md6>
-              <div class="caption black--text">Title</div>
-              <div>{{ personagem.titles }}</div>
-            </v-flex>
-          </v-layout>
-        </v-card>
-      </v-countainer>
-      <v-layout row align="center" justify="space-around">
-        <v-flex xs5 md3>
-          <v-btn @click="getHouse" dark>Houses</v-btn>
-        </v-flex>
-        <v-flex xs12 md3>
-          <v-btn @click="getPerson" color="grey">Characters</v-btn>
-        </v-flex>
-      </v-layout>
+        <v-layout row align="center" justify="center">
+          <v-flex xs5 md6>
+            <v-btn outline block @click="getHouse" dark>Houses</v-btn>
+          </v-flex>
+          
+          <v-flex xs6 md6>
+            <v-btn outline block @click="getPerson" color="grey"
+              >Characters</v-btn
+            >
+          </v-flex>
+        </v-layout>
+        <v-row align="center" justify="center">
+          <v-col  cols="6" >
+            <v-card
+              flat
+              class="pa-2"
+              outlined tile
+              v-for="(casa, i) in casas"
+              :key="i"
+              color="#90A4AE"
+            >
+              <v-spacer></v-spacer>
+              <v-layout :class="`pa-3 casa ${casa.name}`">
+                <v-flex xs6 md6>
+                  <div class="caption black--text">House Name</div>
+                  <div>{{ casa.name }}</div>
+                </v-flex>
+                <v-flex xs6 md6>
+                  <div class="caption black--text">Region Name</div>
+                  <div>{{ casa.region }}</div>
+                </v-flex>
+              </v-layout>
+              <v-divider></v-divider>
+            </v-card>
+          </v-col>
+
+          <v-col  cols="6">
+            <v-card
+              flat
+              class="pa-2"
+              outlined tile
+              v-for="(personagem, i) in personagens"
+              :key="i"
+              color="#B0BEC5"
+            >
+              <v-layout :class="`pa-3 personagem ${personagem.name}`">
+                <v-flex xs6 md6>
+                  <div dark class="caption black--text">Characters</div>
+                  <div>{{ personagem.name }}</div>
+                </v-flex>
+                <v-flex xs6  md6>
+                  <div class="caption black--text">Title</div>
+                  <div>{{ personagem.titles }}</div>
+                </v-flex>
+              </v-layout>
+              <v-divider></v-divider>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
     </v-app>
   </div>
 </template>
@@ -105,5 +97,6 @@ export default {
 </script>
 
 <style>
-
 </style>
+
+class="grey lighten-2 fill-height d-flex flex-column justify-center align-center
