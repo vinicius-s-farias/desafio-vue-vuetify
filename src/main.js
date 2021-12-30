8eb949161ca01555d38f2a91584dfb5e3ad46fc7
guuicarl <guilherme.carlos014@gmail.com>
2021-12-30 14:34:36 -0300

    first commit

--- src/main.js
+++ src/main.js
@@ -2,8 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import router from './router'
+import money from 'v-money'
 
 Vue.config.productionTip = false
+Vue.use(money, {precision: 4})
 
 new Vue({
   vuetify,
