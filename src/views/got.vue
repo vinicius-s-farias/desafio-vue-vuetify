5b5eb5ffb55d98ed88372ac0d6ffae256f1f1cce
vinicius-s-farias <vifarias47@gmail.com>
2022-01-03 14:02:01 -0300

    commit final

--- src/views/got.vue
+++ src/views/got.vue
@@ -1,180 +1,173 @@
 <template>
-  <div class="Projects">
-    <v-card>
-      <v-tabs color="#64FFDA">
-        <v-row justify="space-around">
-          <v-col cols="6" md="4">
-            <v-dialog transition="dialog-top-transition" max-width="600">
-              <template v-slot:activator="{ on, attrs }">
-                <v-btn outline block color="primary" v-bind="attrs" v-on="on">Houses</v-btn>
-              </template>
-              <template v-slot:default="dialog">
-                <v-card>
-                  <v-toolbar color="primary" dark>Who Your 's House</v-toolbar>
-                  <v-card-text >
-                    <v-form class="px-3"
-                     >
-                      <v-text-field
-                        label="House"
-                        prepend-icon=""
-                        v-model="nameH"
+  <div id="inspire">
+    <v-img
+      background-size:
+      cover
+      src="https://s1.1zoom.me/big3/533/Game_of_Thrones_8_Sitting_Three_3_562310_3840x2160.jpg"
+    >
+      <v-container>
+        <v-card justify="center" align="center" class="back-card">
+          <h1 class="mx-auto mt-7" justify="center">Who you are in G.O.T ??</h1>
+          <v-row justify="space-around">
+            <v-col cols="6" md="4">
+              <v-dialog transition="dialog-top-transition" max-width="600">
+                <template v-slot:activator="{ on, attrs }">
+                  <v-btn
+                    outline
+                    block
+                    dark
+                    color="#424242"
+                    v-bind="attrs"
+                    v-on="on"
+                    >Choose your House</v-btn
+                  >
+                </template>
+                <template v-slot:default="dialog">
+                  <v-card>
+                    <v-toolbar color="#757575" dark>
+                      <v-toolbar-title dark align="center">What Your 's House</v-toolbar-title>
+                    </v-toolbar>
+                    <v-card-text>
+                      <v-form class="px-3">
+                        <v-text-field
+                          dakr
+                          label="House"
+                          prepend-icon=""
+                          v-model="nameH"
+                        >
+                        </v-text-field>
+                        <v-text-field
+                          label="Region"
+                          v-model="region"
+                        ></v-text-field>
+                      </v-form>
+                    </v-card-text>
+                    <v-card-actions class="justify-space-between">
+                      <v-btn
+                        flat
+                        class="sucsses mx-0 mt-3"
+                        justify="left"
+                        @click="getHouse"
+                        >Chose</v-btn
                       >
-                      </v-text-field>
-                      <v-text-field 
-                      label="Region"
-                      v-model="region"
-                      ></v-text-field>
-                    </v-form>
-                  </v-card-text>
-                  <v-card-actions class="justify-space-between">
-                    <v-btn  flat class="sucsses mx-0 mt-3" justify="left"
-                    @click="getHouse">Chose</v-btn>
-                    <v-btn text @click="dialog.value = false">Close</v-btn>
-                  </v-card-actions>
-                </v-card>
-              </template>
-            </v-dialog>
-          </v-col>
-          <v-col cols="6" md="4">
-            <v-dialog transition="dialog-top-transition" max-width="600">
-              <template v-slot:activator="{ on, attrs }">
-                <v-btn outline block color="primary" v-bind="attrs" v-on="on">Characters</v-btn>
-              </template>
-              <template v-slot:default="dialog">
-                <v-card>
-                  <v-toolbar color="primary" dark>Who Your 's Characters</v-toolbar>
-                  <v-card-text>
-                    <v-form class="px-3">
-                      <v-text-field
-                        label="Personagem"
-                        v-model="nameC"
-                        prepend-icon=""
-                      ></v-text-field>
-                      <v-text-field
-                        label="Title"
-                        v-model="title"
-                        prepend-icon=""
-                      ></v-text-field>
-                    </v-form>
-                  </v-card-text>
-                  <v-card-actions class="justify-space-between">
-                    <v-btn flat class="sucsses mx-0 mt-3" justify="left" @click="getPerson" >Chose</v-btn>
-                    <v-btn text @click="dialog.value = false">Close</v-btn>
-                  </v-card-actions>
-                </v-card>
-              </template>
-            </v-dialog>
-          </v-col>
-          <v-col cols="6" md="4">
-            <v-dialog transition="dialog-top-transition" max-width="600">
-              <template v-slot:activator="{ on, attrs }">
-                <v-btn  outline block color="primary" v-bind="attrs" v-on="on">Book</v-btn>
-              </template>
-              <template v-slot:default="dialog">
-                <v-card>
-                  <v-toolbar color="primary" dark>Who Your 's Book</v-toolbar>
-                  <v-card-text>
-                    <v-form class="px-3">
-                      <v-text-field
-                        label="Book Name"
-                        v-model="book"
-                        prepend-icon=""
-                      ></v-text-field>
-                      <v-text-field
-                        label="Author"
-                        v-model="author"
-                        prepend-icon=""
-                      ></v-text-field>
-                    </v-form>
-                  </v-card-text>
-                  <v-card-actions class="justify-space-between">
-                    <v-btn flat class="sucsses mx-0 mt-3" justify="left" @click="getBook">Chose</v-btn>
-                    <v-btn text @click="dialog.value = false">Close</v-btn>
-                  </v-card-actions>
-                </v-card>
-              </template>
-            </v-dialog>
-          </v-col>
-        </v-row>
-      </v-tabs>
-    </v-card>
-   
-    <v-app id="inspire">
-      <v-container class="#CFD8DC">
-        <v-spacer></v-spacer>
-        <v-layout row align="center" justify="center">
-          
-        </v-layout>
-        <v-row align="center" justify="center">
-          <!-- <v-col cols="6">
-            <v-card
-              flat
-              class="pa-2"
-              outlined
-              tile
-              v-for="(casa, i) in casas"
-              :key="i"
-              color="#90A4AE"
-            >
-              <v-spacer></v-spacer>
-              <v-layout :class="`pa-3 casa ${casa.name}`">
-                <v-flex xs6 md6>
-                  <div class="caption black--text">House Name</div>
-                  <div>{{ casa.name }}</div>
-                </v-flex>
-                <v-flex xs6 md6>
-                  <div class="caption black--text">Region Name</div>
-                  <div>{{ casa.region }}</div>
-                </v-flex>
-              </v-layout>
-              <v-divider></v-divider>
-            </v-card>
-          </v-col> -->
-
-          <!-- <v-col cols="6">
-            <v-card
-              flat
-              class="pa-2"
-              outlined
-              tile
-              color="#B0BEC5"
-            >
-              <v-layout :class="pa-3 ">
-                <v-flex xs6 md6>
-                  <div dark class="caption black--text">Characters</div>
-                  <div>{{ personagem.name }}</div>
-                </v-flex>
-                <v-flex xs6 md6>
-                  <div class="caption black--text">Title</div>
-                  <div>{{ personagem.titles }}</div>
-                </v-flex>
-              </v-layout>
-              <v-divider></v-divider>
-            </v-card> -->
-          <!-- </v-col> -->
-        </v-row>
+                      <v-btn text @click="dialog.value = false">Close</v-btn>
+                    </v-card-actions>
+                  </v-card>
+                </template>
+              </v-dialog>
+            </v-col>
+            <v-col cols="6" md="4">
+              <v-dialog transition="dialog-top-transition" max-width="600">
+                <template v-slot:activator="{ on, attrs }">
+                  <v-btn
+                    outline
+                    block
+                    dark
+                    color="#424242"
+                    v-bind="attrs"
+                    v-on="on"
+                    >Choose your Characters</v-btn
+                  >
+                </template>
+                <template v-slot:default="dialog">
+                  <v-card>
+                    <v-toolbar color="#757575">
+                      <v-toolbar-title dark
+                        >Who your's Characters</v-toolbar-title
+                      >
+                    </v-toolbar>
+                    <v-card-text>
+                      <v-form class="px-3">
+                        <v-text-field
+                          label="Personagem"
+                          v-model="nameC"
+                          prepend-icon=""
+                        ></v-text-field>
+                        <v-text-field
+                          label="Title"
+                          v-model="title"
+                          prepend-icon=""
+                        ></v-text-field>
+                      </v-form>
+                    </v-card-text>
+                    <v-card-actions class="justify-space-between">
+                      <v-btn
+                        flat
+                        class="sucsses mx-0 mt-3"
+                        justify="left"
+                        @click="getPerson"
+                        >Chose</v-btn
+                      >
+                      <v-btn text @click="dialog.value = false">Close</v-btn>
+                    </v-card-actions>
+                  </v-card>
+                </template>
+              </v-dialog>
+            </v-col>
+            <v-col cols="6" md="4">
+              <v-dialog transition="dialog-top-transition" max-width="600">
+                <template v-slot:activator="{ on, attrs }">
+                  <v-btn
+                    ooutline
+                    block
+                    dark
+                    color="#424242"
+                    v-bind="attrs"
+                    v-on="on"
+                    >Choose your Book</v-btn
+                  >
+                </template>
+                <template v-slot:default="dialog">
+                  <v-card>
+                    <v-toolbar color="#757575">
+                      <v-toolbar-title dark>What Your 's Book</v-toolbar-title>
+                    </v-toolbar>
+                    <v-card-text>
+                      <v-form class="px-3">
+                        <v-text-field
+                          label="Book Name"
+                          v-model="book"
+                          prepend-icon=""
+                        ></v-text-field>
+                        <v-text-field
+                          label="Author"
+                          v-model="author"
+                          prepend-icon=""
+                        ></v-text-field>
+                      </v-form>
+                    </v-card-text>
+                    <v-card-actions class="justify-space-between">
+                      <v-btn
+                        flat
+                        class="sucsses mx-0 mt-3"
+                        justify="left"
+                        @click="getBook"
+                        >Chose</v-btn
+                      >
+                      <v-btn text @click="dialog.value = false">Close</v-btn>
+                    </v-card-actions>
+                  </v-card>
+                </template>
+              </v-dialog>
+            </v-col>
+          </v-row>
+        </v-card>
       </v-container>
-    </v-app>
+    </v-img>
   </div>
 </template>
 
 <script>
 export default {
-  name: "App",
-
   data: () => ({
-
     region: "",
-    nameH :"",
-    nameC:"",
+    nameH: "",
+    nameC: "",
     title: "",
-    author:"",
-    book:""
+    author: "",
+    book: "",
   }),
   methods: {
-    sortBy(prop) {
-      this.projects.sort((a, b) => (a[prop] < b[prop] ? -1 : 1));
-    },
     async getHouse() {
       let info;
       const i = Math.floor(Math.random() * 378);
@@ -188,73 +181,43 @@ export default {
       let info;
       const i = Math.floor(Math.random() * 583);
       const D = await fetch(
-        `https://anapioficeandfire.com/api/characters/${i}`);
+        `https://anapioficeandfire.com/api/characters/${i}`
+      );
       info = await D.json();
-      this.nameC = info.name
-      this.title = info.titles
+      this.nameC = info.name;
+      this.title = info.titles;
     },
     async getBook() {
       let info;
       const i = Math.floor(Math.random() * 12);
       const D = await fetch(`https://anapioficeandfire.com/api/books/${i}`);
       info = await D.json();
-      this.author = info.authors
-      this.book = info.name
-    },   
-    
+      this.author = info.authors;
+      this.book = info.name;
+    },
   },
 };
 </script>
 
 <style>
 .back {
-  background: url('https://s1.1zoom.me/big3/533/Game_of_Thrones_8_Sitting_Three_3_562310_3840x2160.jpgg') no-repeat center center fixed !important;
+  background-image: url("https://s1.1zoom.me/big3/533/Game_of_Thrones_8_Sitting_Three_3_562310_3840x2160.jpg");
   background-size: cover;
 }
+
+.back-card {
+  background-image: url("https://s1.1zoom.me/big3/533/Game_of_Thrones_8_Sitting_Three_3_562310_3840x2160.jpg");
+  background-size: cover;
+}
+h1 {
+  color: white;
+  font-family: Comic Sans MS, Comic Sans, cursive;
+  font-size: 60px;
+  font-weight: 600;
+  margin-top: 1px;
+  margin-bottom: 1px;
+  text-align: center;
+}
+
 </style>
 
-class="grey lighten-2 fill-height d-flex flex-column justify-center align-center
-
-<v-flex >
-            <v-btn class="p -1" outline block @click="getHouse" dark>Houses</v-btn>
-            <!-- <v-btn </v-btn
-            > -->
-            
-          </v-flex>
-          
-           <!-- <v-card>
-      <v-tabs color="#64FFDA">
-         <v-row>        
-      <v-col
-       cols="6"
-        md="4"
-      ><v-flex >
-            <v-btn outline block @click="getHouse" dark>Houses</v-btn>
-          </v-flex></v-col>
-       <v-col
-        cols="6"
-        md="4"
-      ><v-flex >
-            <v-btn outline block @click="getPerson" color="grey">Characters</v-btn>
-        </v-flex>
-            </v-col>     
-       <v-col
-        cols="6"
-        md="4"
-      ><v-flex >
-            <v-btn outline block @click="getBook" dark>Books</v-btn>
-          </v-flex>
-      </v-col>
-    </v-row>
-      </v-tabs>
-    </v-card> -->
-
-    <!-- <v-flex >
-            <v-btn outline block @click="getHouse" dark>Houses</v-btn>
-          </v-flex>
-          
-          <v-flex >
-            <v-btn outline block @click="getPerson" color="grey"
-              >Characters</v-btn
-            >
-          </v-flex> -->
