bfc9edeccaf9ee2bac973a3478ad1ed9560fa5f4
Juliana <julianasouzamelo@live.com>
2022-01-03 14:22:49 -0300

    adicionar avatar dos integrantes do time

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,33 +1,70 @@
 <template>
-  <v-container>
-    <v-layout row wrap>
-      <v-flex xs12 sm6 md4 lg3 v-bind="team" v-for="person in newTeam" :key="person.name">
-        <v-card text class="text-xs-center ma-3">
-          <v-responsive class="pt-4">
-              <v-avatar size="100">
-                  <img :src="person.link">
-              </v-avatar>
-          </v-responsive>
-          <v-card-text>
-            <div class="subheading">{{ person.name }}</div>
-            <div class="grey--text">{{ person.name }}</div>
-          </v-card-text>
-          <v-card-actions>
-            <v-btn text color="grey">
-              <v-icon small left> message</v-icon>
-              <span> Message </span>
-            </v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-flex>
-    </v-layout>
-  </v-container>
+  <div class="team">
+    <v-container>
+      <h1 class="subheading grey--text">Equipe</h1>
+      <v-spacer></v-spacer>
+      <v-layout row wrap>
+        <v-flex xs12 sm6 md4 lg3 v-for="person in newTeam" :key="person.name">
+          <v-card text class="text-xs-center ma-3" max-height="300">
+            <v-responsive class="pt-4">
+              <v-row justify="space-around">
+                <v-avatar size="100" class="grey lighten-2 pa-2 ma-4">
+                  <img :src="person.link" />
+                </v-avatar>
+              </v-row>
+            </v-responsive>
+            <v-card-text>
+              <div class="subheading">{{ person.name }}</div>
+              <div class="grey--text">{{ person.role }}</div>
+            </v-card-text>
+            <v-card-actions>
+              <v-dialog
+      v-model="dialog"
+      persistent
+      max-width="290"
+    >
+      <template v-slot:activator="{ on, attrs }">
+        <v-btn
+          color="primary"
+          dark
+          v-bind="attrs"
+          v-on="on"
+        >
+          Envie uma mensagem
+        </v-btn>
+      </template>
+      <v-card>
+        <v-textarea label="Envie uma mensagem aos nossos desenvolvedores!">
+        </v-textarea>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn
+            color="green darken-1"
+            text
+            @click="dialog = false"
+          >
+            Disagree
+          </v-btn>
+          <v-btn
+            color="green darken-1"
+            text
+            @click="dialog = false"
+          >
+            Agree
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+            </v-card-actions>
+          </v-card>
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </div>
 </template>
 
 
 <script>
-import axios from "axios";
-
 export default {
   data() {
     return {
@@ -38,35 +75,43 @@ export default {
         "pauleramdf",
         "vinicius-s-farias",
       ],
+      dialog: false,
       avatarLink: "",
       newTeam: [],
+      success: true,
+      message: ''
     };
   },
   mounted() {
     this.loadAvatar();
+    this.thankAlert();
   },
   methods: {
     loadAvatar() {
-      let avatarLink;
       const team = [
-        { name: "Guilherme Carlos", role: "Dev", link: ''},
-        { name: "Juliana Souza", role: "Dev", link: ''},
-        { name: "Leo Santana", role: "Dev", link: ''},
-        { name: "Paulo Fernandes", role: "Dev", link: ''},
-        { name: "Vinicius Farias", role: "Dev", link: ''},
+        { name: "Guilherme Carlos", role: "Dev", link: "https://avatars.githubusercontent.com/u/91094202?v=4" },
+        { name: "Juliana Souza", role: "Dev", link: "https://avatars.githubusercontent.com/u/64979669?v=4 " },
+        { name: "Leo Santana", role: "Dev", link: "https://avatars.githubusercontent.com/u/17907755?v=4" },
+        { name: "Paulo Fernandes", role: "Dev", link: "https://avatars.githubusercontent.com/u/36416509?v=4" },
+        { name: "Vinicius Farias", role: "Dev", link: "https://avatars.githubusercontent.com/u/71359046?v=4" },
       ];
-      for (let i = 0; i < this.userNames.length; i++) {
+      /*for (let i = 0; i < this.userNames.length; i++) {
         axios
           .get(`https://api.github.com/users/${this.userNames[i]}`)
           .then((response) => {
             avatarLink = response.data.avatar_url;
-            team[i].link = avatarLink
-            console.log(team[i].link)
-          }).catch((e) => {
-            console.log(e, 'erro')
+            team[i].link = avatarLink;
+            console.log(team[i].link);
+          })
+          .catch((e) => {
+            console.log(e, "erro"); 
           });
-      }
+      }*/
       this.newTeam = team;
+    },
+
+    thankAlert() {
+      this.success = !this.success;
     },
   },
 };
--- src/views/Streaming.vue
+++ src/views/Streaming.vue
@@ -1,13 +1,20 @@
 <template>
   <v-container>
-    <v-card class="pa-md-16 mx-lg-auto" width="800px">
+    <v-card class="pa-md-16 mx-lg-auto" width="800px" color="#ECEFF1" >
       <v-card-title>
-        Descubra onde assistir seu filme ou série favoritos
+        <h3>Descubra onde assistir seu filme ou série favoritos!</h3>
       </v-card-title>
-        <v-col cols="6">
-            <v-img src="https://cdn.watchmode.com/misc_images/homepageHeaderLogosText.png"></v-img>
-          </v-col>
-      <v-form >
+      <v-col cols="8">
+        <v-img
+          :src="image"
+          id="rounded-card"
+          max-height="300"
+          max-width="500"
+          :aspect-ratio="16 / 9"
+        >
+        </v-img>
+      </v-col>
+      <v-form>
         <v-row>
           <v-col cols="12">
             <v-text-field
@@ -18,28 +25,36 @@
             >
               >
             </v-text-field>
-            <v-alert
-              v-if="erro != ''"
-              :type="alertType"
-              color="red lighten-2"
-            >
-            Título não encontrado, tente novamente
+            <v-alert v-if="erro != ''" :type="alertType" color="red lighten-2">
+              Título não encontrado, tente novamente
             </v-alert>
             <v-btn label="Pesquisar" @click="getId"> Pesquisar </v-btn>
           </v-col>
           <v-card-text> Onde assistir: </v-card-text>
           <v-col cols="12">
-            <v-text-field prepend-icon="mdi-laptop" label="Web" v-model="webUrl"></v-text-field>
+            <v-text-field
+              prepend-icon="mdi-laptop"
+              label="Web"
+              v-model="webUrl"
+            ></v-text-field>
             <v-btn v-if="exibirWeb" v-bind:href="webUrl"> Acessar </v-btn>
             <v-spacer></v-spacer>
-            <v-text-field prepend-icon="mdi-apple" label="iOS" v-model="iosUrl"></v-text-field>
+            <v-text-field
+              prepend-icon="mdi-apple"
+              label="iOS"
+              v-model="iosUrl"
+            ></v-text-field>
             <v-btn v-if="exibirIos" v-bind:href="iosUrl"> Acessar </v-btn>
             <v-spacer></v-spacer>
-            <v-text-field prepend-icon="mdi-android" label="Android" v-model="androidUrl"></v-text-field>
+            <v-text-field
+              prepend-icon="mdi-android"
+              label="Android"
+              v-model="androidUrl"
+            ></v-text-field>
             <v-btn v-if="exibirAndroid" v-bind:href="androidUrl">
               Acessar
             </v-btn>
-          </v-col> 
+          </v-col>
         </v-row>
       </v-form>
     </v-card>
@@ -54,6 +69,8 @@ export default {
     return {
       title: "",
       API_KEY: "j6jiHjylx8oFwsqwo0OglkaaFekIzE7ZT4R0ZjIl",
+      image:
+        "https://thumbs.dreamstime.com/b/mulher-africana-e-garota-assistindo-tv-com-lanche-m%C3%A3e-filha-feliz-fam%C3%ADlia-negra-filme-de-cartoons-ou-futebol-pipoca-copiar-168627286.jpg",
       id: "",
       webUrl: "",
       iosUrl: "",
@@ -67,52 +84,57 @@ export default {
   },
   methods: {
     getTitle(id) {
-      this.erro = ''
+      this.erro = "";
       console.log(id);
-        axios
-          .get(
-            `https://api.watchmode.com/v1/title/${id}/sources/?apiKey=${this.API_KEY}`
-          )
-          .then((response) => {
-            this.androidUrl = response.data[10].android_url;
-            if (!this.androidUrl) {
-              this.androidUrl = "Indisponível";
-            } else {
-              this.exibirAndroid = true;
-            }
-            this.webUrl = response.data[10].web_url;
-            if (!this.webUrl) {
-              this.webUrl = "Indisponível";
-            } else {
-              this.exibirWeb = true;
-            }
-            this.iosUrl = response.data[10].ios_url;
-            if (!this.iosUrl) {
-              this.iosUrl = "Indisponível";
-            } else {
-              this.exibirIos = true;
-            }
-          }).catch((e) => {
-            this.erro = e;
-            this.alertInput()
-          })
-      },
+      axios
+        .get(
+          `https://api.watchmode.com/v1/title/${id}/sources/?apiKey=${this.API_KEY}`
+        )
+        .then((response) => {
+          this.androidUrl = response.data[10].android_url;
+          if (!this.androidUrl) {
+            this.androidUrl = "Indisponível";
+          } else {
+            this.exibirAndroid = true;
+          }
+          this.webUrl = response.data[10].web_url;
+          if (!this.webUrl) {
+            this.webUrl = "Indisponível";
+          } else {
+            this.exibirWeb = true;
+          }
+          this.iosUrl = response.data[10].ios_url;
+          if (!this.iosUrl) {
+            this.iosUrl = "Indisponível";
+          } else {
+            this.exibirIos = true;
+          }
+        })
+        .catch((e) => {
+          this.erro = e;
+          this.alertInput();
+          this.eraseError();
+        });
+    },
 
     getId() {
-      this.erro = ''
+      this.erro = "";
       const titleFormat = this.title.replaceAll(" ", "%20");
-        axios
-          .get(
-            `https://api.watchmode.com/v1/search/?apiKey=${this.API_KEY}&search_field=name&search_value=${titleFormat}`
-          )
-          .then((response) => {
-            this.id = response.data.title_results[0].id;
-            this.getTitle(this.id);
-            console.log(response.data);
-          }).catch((e) => {
-            this.erro = e;
-            this.alertInput()
-          })
+      axios
+        .get(
+          `https://api.watchmode.com/v1/search/?apiKey=${this.API_KEY}&search_field=name&search_value=${titleFormat}`
+        )
+        .then((response) => {
+          this.id = response.data.title_results[0].id;
+          this.getTitle(this.id);
+          console.log(response.data);
+        })
+        .catch((e) => {
+          this.erro = e;
+          this.alertInput();
+          this.eraseError();
+          console.log(this.erro)
+        });
     },
 
     checkForm() {
@@ -131,7 +153,24 @@ export default {
         this.alertType = 'error'
       }
     },
+    
+    eraseError() {
+    if (this.erro) {
+        this.webUrl = "";
+        this.exibirWeb = false;
+        this.iosUrl = "";
+        this.exibirIos = false;
+        this.androidUrl = "";
+        this.exibirAndroid = false;
+    }
   }
-}
 
+  },
+};
 </script>
+
+<style scoped>
+#rounded-card {
+  border-radius: 1%;
+}
+</style>
